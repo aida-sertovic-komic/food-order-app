ebd7c06e4d48e7dc0ac1eda211b2fc9735dfbc30
Aida Šertović Komić <sertovicaida@gmail.com>
2023-02-03 15:39:56 +0100

    restructure meal item

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,39 +1,48 @@
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealsItem/MealsItem';
 
 const DUMMY_MEALS = [
     {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
+        id: 'm1',
+        name: 'Sushi',
+        description: 'Finest fish and veggies',
+        price: 22.99,
     },
     {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
+        id: 'm2',
+        name: 'Schnitzel',
+        description: 'A german specialty!',
+        price: 16.5,
     },
     {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
+        id: 'm3',
+        name: 'Barbecue Burger',
+        description: 'American, raw, meaty',
+        price: 12.99,
     },
     {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
+        id: 'm4',
+        name: 'Green Bowl',
+        description: 'Healthy...and green...',
+        price: 18.99,
     },
-  ];
+];
 
 const AvailableMeals = () => {
-const meals = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>)
-    return(
+    const meals = DUMMY_MEALS.map((meal) =>
+        <MealItem
+            key={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+        />)
+    return (
         <section className={classes.meals}>
             <Card>
-                {meals}
+                <ul>
+                    {meals}
+                </ul>
             </Card>
         </section>
     )

61a7755be9327811b1a8e470a63533f3f96bc9b2
Aida Šertović Komić <sertovicaida@gmail.com>
2023-02-06 15:57:13 +0100

    manage cart state

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import classes from './Header.module.css';
 import { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
+
 const Header = (props) => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Meals</h1>
-                <HeaderCartButton />
+                <HeaderCartButton onClick={props.onShowCart} />
             </header>
             <div>
                 <img className={classes['main-image']} src={mealsImage} alt='A table full of delicious food!' />

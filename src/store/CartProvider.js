506b381866c314962f52183d136603e81fadae67
Aida Šertović Komić <sertovicaida@gmail.com>
2023-02-14 16:05:14 +0100

    update cart reducer

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -6,9 +6,31 @@ const defaultCartState = {
     totalAmount: 0
 }
 const cartReducer = (state, action) => {
-    if(action.type  === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
+    if (action.type === 'ADD') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        const existingCartItemIndex = state.items.findIndex(
+            item => item.id === action.item.id
+        );
+
+        const existingCartItem = state.items[existingCartItemIndex];
+
+        let updatedItem;
+        let updatedItems;
+
+        if (existingCartItem) {
+            updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            }
+            updatedItems = [...state.items]
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+        else {
+            updatedItem = { ...action.item };
+            updatedItems = state.items.concat(action.item);
+        }
+
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -19,16 +41,16 @@ const cartReducer = (state, action) => {
 
 
 const CartProvider = props => {
-    const [cartState, dispatchCartAction ] = useReducer(cartReducer, defaultCartState);
-    const addItemToCartHandler = item => { 
-        dispatchCartAction({type: 'ADD', item })
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
+    const addItemToCartHandler = item => {
+        dispatchCartAction({ type: 'ADD', item })
     };
-    const removeItemFromCartHandler = (id) => { 
-        return dispatchCartAction({type: 'REMOVE', id: id})
+    const removeItemFromCartHandler = (id) => {
+        return dispatchCartAction({ type: 'REMOVE', id: id })
     };
 
     const cartContext = {
-        items:  cartState.items,
+        items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemFromCartHandler
